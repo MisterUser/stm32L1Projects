d78e7207d3db4449545157df7d30c16a2b1dfea5
MrUser <gregknoll@gmail.com>
2015-08-05 20:59:41 +0200

    Now both external DACs functional 	modified:   implementations/extern_DAC.c

--- Function_Gen/src/implementations/extern_DAC.c
+++ Function_Gen/src/implementations/extern_DAC.c
@@ -1,16 +1,19 @@
 #include "extern_DAC.h"
 
+//------------Functions-----------------//
 extern const uint8_t fourBitSine_16[16];
 const uint8_t fourBitTri_10[10]={0,3,6,9,12,15,12,9,6,3};
 const uint8_t fourBitRamp_6[6]={0,3,6,9,12,15};
+
+
+//------Local variables------//
 const uint8_t* dac1_array_ptr;
 uint8_t sizeOfarray1;
 uint8_t dac1_array_current;
+
 const uint8_t* dac2_array_ptr;
 uint8_t sizeOfarray2;
 uint8_t dac2_array_current;
-volatile uint16_t setMask;
-volatile uint16_t resetMask;
 
 
 GPIO_InitTypeDef GPIO_InitStructure;
@@ -34,7 +37,7 @@ void external_DAC_setup(void)
    //Timer
    RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM9, ENABLE);
 
-   /* TIM IT enable */
+   /* TIM IT disable */
    TIM_ITConfig(TIM9, TIM_IT_Update, DISABLE);
 
    /* -----------NVIC Configuration ------------*/
@@ -75,11 +78,42 @@ void external_DAC_setup(void)
    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_40MHz ;
    GPIO_Init(ExtDAC2_PORT, &GPIO_InitStructure);
+
+   //Timer
+   RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM10, ENABLE);
+
+   /* TIM IT disable */
+   TIM_ITConfig(TIM10, TIM_IT_Update, DISABLE);
+
+   /* -----------NVIC Configuration ------------*/
+   /* Enable the TIM10 global Interrupt */
+   NVIC_InitStructure.NVIC_IRQChannel = TIM10_IRQn;
+   NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+   NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+   NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+   NVIC_Init(&NVIC_InitStructure);
+
+   /* Time base configuration */
+   TIM_TimeBaseStructure.TIM_Period = 10;
+   TIM_TimeBaseStructure.TIM_Prescaler = 25;
+   TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV4;
+   TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+
+   TIM_TimeBaseInit(TIM10, &TIM_TimeBaseStructure);
+
+   dac2_array_ptr=&fourBitTri_10[0];
+   dac2_array_current=0;
+   sizeOfarray2 = 10;
+
+   /* TIM enable counter */
+   TIM_Cmd(TIM10, ENABLE);
+
+   /* TIM IT enable */
+   TIM_ITConfig(TIM10, TIM_IT_Update, ENABLE);
 }
 
 void TIM9_IRQHandler(void)
 {
-  static int pinVal=0;
   if (TIM_GetITStatus(TIM9, TIM_IT_Update) != RESET)
   {
     TIM_ClearITPendingBit(TIM9, TIM_IT_Update);
@@ -99,3 +133,17 @@ void TIM9_IRQHandler(void)
     if(dac1_array_current>=sizeOfarray1){dac1_array_current = 0;}
   } 
 }
+
+void TIM10_IRQHandler(void)
+{
+  if(TIM_GetITStatus(TIM10, TIM_IT_Update) != RESET)
+  {
+    TIM_ClearITPendingBit(TIM10, TIM_IT_Update);
+
+    //Set pins according to array
+    ExtDAC_setPin   = ((  dac2_array_ptr[dac2_array_current])&0x0F) << ExtDAC2_PortOffset;
+    ExtDAC_resetPin = ((~(dac2_array_ptr[dac2_array_current++]))&0x0F) << ExtDAC2_PortOffset;
+    if(dac2_array_current>=sizeOfarray2){dac2_array_current = 0;}
+  }
+}
+
